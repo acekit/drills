b16b09f7bc4a45e1433ebcc601afb9c65bc732d3
acekit <110akito@gmail.com>
2020-12-15 18:36:43 +0900

    Add files via upload

--- drill01.c
+++ drill01.c
@@ -8,36 +8,53 @@ int N_FUNCTION = 4;
 2つの数値を入力すると、加減乗除結果を表示する。
 加減乗除専用の関数を作成すること。
 */
-float cal_four_arithmetic_operations(float a, float b, int mode)
+
+/*
+float a       :先方の入力値
+float b       :後方の入力値
+int mode      :1.+ , 2.- , 3.* , 4./ 
+int on_print  :printfあり、なし
+*/
+float cal_four_arithmetic_operations(float a, float b, int mode, int on_print)
 {
   float result = 0;
+  char ope;
   switch (mode)
   {
   case 0 /* value */:
     result = a + b;
+    ope = '+';
     //printf("A+B=%f \n", result);
     break;
   case 1 /* value */:
     result = a - b;
+    ope = '-';
     //printf("A-B=%f \n", result);
     break;
   case 2 /* value */:
     result = a * b;
+    ope = '*';
     //printf("A*B=%f \n", result);
     break;
   case 3 /* value */:
     if (b == 0)
     {
       result = 0;
+      ope = 'E';
     }
     else
     {
       result = a / b;
+      ope = '/';
     }
-    //printf("A/B=%f \n", result);
     break;
   default:
+    ope = 'E';
     break;
+  }
+  if (on_print)
+  {
+    printf("A%cB=%f \n", ope, result);
   }
   return result;
 }
@@ -46,23 +63,24 @@ int main(void)
 {
   //input from stdin a,b
   float input[BUFF_LENGTH];
-  float *p_input = input;
   float buff[3];
   for (int i = 0; i < N_INPUT; i++)
   {
-    printf("%s\n", "Please, input 2 numbers.");
+    printf("%s%d%s\n", "Please, input ", N_INPUT - i, "numbers.");
     scanf("%f", &input[i]);
     printf("input is %f \n", input[i]);
   }
+  (void)getchar(); //'¥n'の読み捨て
+
   //printf +,-,*,/
-  float val;
+  float val = 0;
   for (int i = 0; i < N_FUNCTION; i++)
   {
-    val = cal_four_arithmetic_operations(p_input[0], p_input[1], i);
-  //  printf("%f \n", val);
+    val = cal_four_arithmetic_operations(input[0], input[1], i, 1);
+    printf("%f \n", (float)val);
   }
-  printf("%s \n", "Please, push enter key.");
-  scanf("%c", &buff[0]);
-
+  printf("%s \n", "Please, push any key.");
+  // scanf("%c", &buff[0]);
+  (void)getchar();
   return 0;
 }
--- drill02.c
+++ drill02.c
@@ -4,8 +4,11 @@
 /*
 2 じゃんけんゲーム
 PCとじゃんけん対戦するゲーム。
-人間はキーを押下し、PCはランダム値により手を決め、結果を判定・表示する。勝率も表示する。
-グー：g,チョキ：c,パー：p
+人間はキーを押下し、
+PCはランダム値により手を決め、
+結果を判定・表示する。
+勝率も表示する。
+グー：g,チョキ：c,パー：p ※あえて文字にしました。効率だけ考えたら1,2,3とかでいいと思います。
 */
 
 int main( void )
@@ -15,19 +18,18 @@ int main( void )
   char user[2];//buffer user's choice ex) g,c,p or e:escape from loop
   srand(time(NULL));
   while(on_loop){
-    while(){
-
-    }
+    printf("Please, select your key from 'g' or 'c' or 'p'.\n");
     fgets(user,2,stdin);//input user's choice
     if ((user[0]=='e')) {
-      printf("%s\n",user);
-      /* code */
+      (void)getchar();
       break;
     }
 
     if(user[0]!='g'&&user[0]!='c'&&user[0]!='p'){
-
+      printf("Please, select from g,c,p.");
     }else{
+      printf("Your choice is :%s\n",user);
+      
       double v_rand;
       v_rand=(double)rand();
       printf("%f\n",v_rand);
@@ -45,7 +47,7 @@ int main( void )
   }*/
 }
 printf("%s\n","Thank you for your playing!" );
-//transfer values to float from char
-//printf +,-,*,/
+printf("%s\n", "Push any keys.");
+(void)getchar();
 return 0;
 }
