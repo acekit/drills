d7713150902dd8fff8bf84f35b7914771e734d56
acekit <110akito@gmail.com>
2021-04-23 20:15:19 +0900

    Update drill09
    Corrected the code for points No. 1 to 4 in TR

--- drill09.c
+++ drill09.c
@@ -13,22 +13,20 @@ No .9 電話帳
 
 //数値定義 =====================
 #define LEN_EMPLOYEE_NUMBER 9  //9桁 入力可能な整数の桁数 030xxxxxx なので9文字
-#define LEN_DATABASE 3         //社員情報データベース内の登録社員数
+#define LEN_EMPLOYEE_EXTENTION 4   //4桁 内線番号
 #define TRUE 1
 #define FALSE 0
 //構造体型宣言
 //社員情報格納用
 typedef struct {
-    unsigned int employee_number;
+    char employee_number[LEN_EMPLOYEE_NUMBER + 1];//null文字分
     char name[20];
-    unsigned int employee_extention;
+    char employee_extention[LEN_EMPLOYEE_EXTENTION + 1]; //null文字分
 }   T_EmployeeData;
 
 // 関数宣言 =====================
-unsigned int GetUserInput();
-unsigned char InitEmployeeInformation( T_EmployeeData* );
-void PrintfTheNameOfEmployee( T_EmployeeData );
-void PrintfEmployeeExtention( T_EmployeeData );
+unsigned char GetUserInput(T_EmployeeData *);
+unsigned char SearchAndGetEmployeeInformationByEmployeeNumber(T_EmployeeData *);
 
 void main(){
     T_EmployeeData user;//社員情報
@@ -37,10 +35,18 @@ void main(){
     //標準入力から社員番号を取得する
     printf("Please, input employee's number.\n");
     printf("If you enter a number greater than %d digits, it will be recognized as a %d-digit number from the beginning.\n", LEN_EMPLOYEE_NUMBER, LEN_EMPLOYEE_NUMBER);
-    user.employee_number = GetUserInput();
+    is_function_succeeded = GetUserInput(&user);
+    //入力文字数が足りなかった時の処理
+    if (is_function_succeeded == FALSE)
+    {
+        printf("Not enough characters to enter.\n");
+        printf("Push enter keys.");
+        getchar(); //画面を残す用
+        return;
+    };
 
-    //社員番号から社員情報を初期化する
-    is_function_succeeded = InitEmployeeInformation( &user );
+    //社員番号から社員情報を検索する
+    is_function_succeeded = SearchAndGetEmployeeInformationByEmployeeNumber(&user);
 
     //データベース上に該当する社員情報が存在しない場合の処理
     if(is_function_succeeded == FALSE){
@@ -50,10 +56,10 @@ void main(){
         return;
     };
 
-    //社員氏名を表示する
-    PrintfTheNameOfEmployee( user );
+    //社員氏名を表示する 
+    printf("This employee's name is %s.\n", user.name);
     //内線番号を表示する
-    PrintfEmployeeExtention( user );
+    printf("This employee's employee extention is %4s.\n", user.employee_extention);
 
     printf("Push enter keys.");
     getchar(); //画面を残す用
@@ -61,62 +67,49 @@ void main(){
 }
 
 /*@brief ユーザーの入力を得る関数           */
-/*@param [in]                           */
-/*@return unsigned int :入力値          */
-unsigned int GetUserInput(){
-    char input_val_char[LEN_EMPLOYEE_NUMBER + 1];
-    unsigned int input_val;
-    scanf("%9s%*[^\n]", &input_val_char); // ここは、セキュリティ面とscanfの関係から、マジックナンバーですが、8を直入れしています。
+/*@param [in] EmployeeData *input_employee_data                         */
+/*@return unsigned char :処理の成否          */
+unsigned char GetUserInput(T_EmployeeData *input_employee_data)
+{
+    unsigned char is_process_succeeded = FALSE;
+    char input_val_char[LEN_EMPLOYEE_NUMBER + 1]; //null文字分
+    scanf("%9s%*[^\n]", &input_val_char); // ここは、セキュリティ面とscanfの関係から、マジックナンバーですが、9を直入れしています。
     while (getchar() != '\n'); // 改行コードの読み飛ばし
-    input_val = atoi(input_val_char);
-    printf("Your input is %09d.\n", input_val);
-    return input_val;
+    printf("Your input is %s.\n", input_val_char);
+    if (strlen(input_val_char)  == LEN_EMPLOYEE_NUMBER){//+1はnull文字分
+        is_process_succeeded = TRUE;
+    }
+    strcpy(input_employee_data->employee_number, input_val_char);
+    return is_process_succeeded;
 }
 
-/*@brief 社員番号から社員情報を初期化する関数         */
-/*@param [in]_EmployeeData *input_employee_data    */
+/*@brief 社員番号から社員情報を検索し、社員情報を取得する関数         */
+/*@param [in] EmployeeData *input_employee_data    */
 /*@return unsigned char :処理の成否                 */
-unsigned char InitEmployeeInformation(T_EmployeeData *input_employee_data){
+unsigned char SearchAndGetEmployeeInformationByEmployeeNumber(T_EmployeeData *input_employee_data)
+{
     unsigned char is_process_succeeded = FALSE;
-    unsigned int tmp_employee_number = input_employee_data->employee_number ;
     unsigned int i = 0;
+    size_t size_of_database;
     //data base の作成
-    T_EmployeeData database_of_employee[LEN_DATABASE] =
+    T_EmployeeData database_of_employee[] =
         {
-            {30164701, "井熊　駿", 0000},
-            {30164702, "伊藤　晃人", 1111},
-            {30164703, "寺澤　翔", 2222},
+            {"030164701", "井熊　駿", "0000"},
+            {"030164702", "伊藤　晃人", "1111"},
+            {"030164703", "寺澤　翔", "2222"},
         };
-
+    size_of_database = sizeof(database_of_employee) / sizeof(database_of_employee[0]);
     //該当情報の探索
-    for (i = 0; i < LEN_DATABASE; i++)
+    for (i = 0; i < size_of_database; i++)
     {
         //社員番号が一致するデータがあれば、値を代入
-        if (database_of_employee[i].employee_number == input_employee_data->employee_number)
+        if (strcmp(database_of_employee[i].employee_number,  input_employee_data->employee_number) == 0)
         {
             strcpy(input_employee_data->name, database_of_employee[i].name);
-            input_employee_data->employee_extention = database_of_employee[i].employee_extention;
+            strcpy(input_employee_data->employee_extention, database_of_employee[i].employee_extention);
             return is_process_succeeded = TRUE;
         }        
     }
 
     return is_process_succeeded;
-}
-
-/*@brief 社員氏名を表示する関数           */
-/*@param [in] T_EmployeeData input_employee_data :社員情報*/
-/*@return           */
-void PrintfTheNameOfEmployee(T_EmployeeData input_employee_data)
-{
-    printf("This employee's name is %s.\n" , input_employee_data.name);
-    return;
-}
-
-/*@brief 内線番号を表示する関数           */
-/*@param [in] T_EmployeeData input_employee_data :社員情報*/
-/*@return           */
-void PrintfEmployeeExtention(T_EmployeeData input_employee_data)
-{
-    printf("This employee's employee extention is %4d.\n" , input_employee_data.employee_extention);
-    return;
 }
